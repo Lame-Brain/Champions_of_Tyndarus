b953e2f0856ce73cf539b38f3dcf6a6efbccec99
The Lame Brain <thelamebrain@gmail.com>
2021-10-16 22:59:46 -0700

    cleaned up the map, looks like my axis are flipped weird.

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -8,7 +8,8 @@ public class MapManager : SerializedMonoBehaviour
     public static MapManager MAP;
 
     public TextAsset csvMap;
-    [HideInInspector] public int[,] terrain_map;
+    [HideInInspector] 
+    public int[,] terrain_map;
 
     public int mapx, mapy;
 
@@ -24,10 +25,10 @@ public class MapManager : SerializedMonoBehaviour
             for(int x = 0; x < mapx; x++)
             {
                 terrain_map[x, y] = IntFromString(firstline[x]);
-                if(y < mapy - 1) firstline = rowlines[0].Split(new char[] { ',' });
+                if(y < mapy - 1) firstline = rowlines[y].Split(new char[] { ',' });
             }
 
-        DrawMap();
+        //DrawMap();
     }
 
     public GameObject bridge_prefab, deepwater_prefab, water_prefab, river_prefab, beach1_prefab, beach2_prefab, beach3_prefab, stone_prefab, dirt_prefab, grass_prefab, sand_prefab;
@@ -36,6 +37,11 @@ public class MapManager : SerializedMonoBehaviour
     private void Awake()
     {
         MAP = this;        
+    }
+
+    private void Start()
+    {
+        DrawMap();
     }
 
     private int IntFromString(string s)
@@ -51,7 +57,6 @@ public class MapManager : SerializedMonoBehaviour
         //east is 0,0,270
         //south is 0,0,180
         //west is 0,0,90
-        Debug.Log(mapx + ", " + mapy);
         for(int y = 0; y < mapy; y++)
             for(int x = 0; x < mapx; x++)
             {
@@ -59,28 +64,49 @@ public class MapManager : SerializedMonoBehaviour
                 if (terrain_map[x, y] == 1) Instantiate(bridge_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 270), this.transform); //bridge east
                 if (terrain_map[x, y] == 2) Instantiate(bridge_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 180), this.transform); //bridge south
                 if (terrain_map[x, y] == 3) Instantiate(bridge_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 90), this.transform); //bridge west
-                if (terrain_map[x, y] == 4) Instantiate(deepwater_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 00), this.transform); //deepwater
-                if (terrain_map[x, y] == 5) Instantiate(beach2_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 270), this.transform); //beach, water nw
-                if (terrain_map[x, y] == 6) Instantiate(dirt_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 270), this.transform); //dirt
-                if (terrain_map[x, y] == 7) Instantiate(grass_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 270), this.transform); //grass
-                if (terrain_map[x, y] == 8) Instantiate(river_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), this.transform); //river north
+                if (terrain_map[x, y] == 4) Instantiate(deepwater_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), this.transform); //deepwater
+                if (terrain_map[x, y] == 6) Instantiate(dirt_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), this.transform); //dirt
+                if (terrain_map[x, y] == 7) Instantiate(grass_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), this.transform); //grass
+                if (terrain_map[x, y] == 8) Instantiate(river_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 180), this.transform); //river north
                 if (terrain_map[x, y] == 9) Instantiate(river_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 270), this.transform); //river east
-                if (terrain_map[x, y] == 10) Instantiate(river_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 180), this.transform); //river south
+                if (terrain_map[x, y] == 10) Instantiate(river_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), this.transform); //river south
                 if (terrain_map[x, y] == 12) Instantiate(river_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 90), this.transform); //river west
                 if (terrain_map[x, y] == 13) Instantiate(sand_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), this.transform); //sand
                 if (terrain_map[x, y] == 14) Instantiate(water_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), this.transform); //shallow water
                 if (terrain_map[x, y] == 15) Instantiate(stone_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), this.transform); //rock
-                if (terrain_map[x, y] == 16) Instantiate(beach1_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 270), this.transform); //beach south
+
+                if (terrain_map[x, y] == 16) Instantiate(beach1_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 90), this.transform); //beach south
+
                 if (terrain_map[x, y] == 18) Instantiate(beach1_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), this.transform); //beach east
-                if (terrain_map[x, y] == 19) Instantiate(beach2_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 180), this.transform); //beach water ne
-                if (terrain_map[x, y] == 20) Instantiate(beach2_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 90), this.transform); //beach water se
-                if (terrain_map[x, y] == 21) Instantiate(beach2_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 180), this.transform); //beach west
-                if (terrain_map[x, y] == 22) Instantiate(beach2_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 90), this.transform); //beach north
-                if (terrain_map[x, y] == 24) Instantiate(beach3_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), this.transform); //beach sand sw
-                if (terrain_map[x, y] == 25) Instantiate(beach3_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 270), this.transform); //beach sand nw
-                if (terrain_map[x, y] == 26) Instantiate(beach3_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 180), this.transform); //beach sand ne
-                if (terrain_map[x, y] == 27) Instantiate(beach3_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 90), this.transform); //beach sand se
-                if (terrain_map[x, y] == 28) Instantiate(beach2_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), this.transform); //beach water sw
+
+                if (terrain_map[x, y] == 21) Instantiate(beach1_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 180), this.transform); //beach west
+
+                if (terrain_map[x, y] == 22) Instantiate(beach1_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 270), this.transform); //beach north
+
+
+                if (terrain_map[x, y] == 19) Instantiate(beach2_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 90), this.transform); //beach water ne
+
+                if (terrain_map[x, y] == 20) Instantiate(beach2_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 180), this.transform); //beach water se
+
+                if (terrain_map[x, y] == 28) Instantiate(beach2_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 270), this.transform); //beach water sw
+
+                if (terrain_map[x, y] == 5) Instantiate(beach2_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), this.transform); //beach, water nw
+
+
+                if (terrain_map[x, y] == 26) Instantiate(beach3_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 90), this.transform); //beach sand ne
+
+                if (terrain_map[x, y] == 27) Instantiate(beach3_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 180), this.transform); //beach sand se
+
+                if (terrain_map[x, y] == 24) Instantiate(beach3_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 270), this.transform); //beach sand sw
+
+                if (terrain_map[x, y] == 25) Instantiate(beach3_prefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), this.transform); //beach sand nw
+
+
+
+                                                                                                                                           //                {
+                                                                                                                                           //                    GameObject _go = 
+                                                                                                                                           //                    Debug.Log("Here is one", _go);
+                                                                                                                                           //                }
             }
     }
 }
